0e2f01b07b5177aceb7a64a504a874e9f9c898ac
Anthooooooooo <anthonytoons7818@gmail.com>
2026-01-03 02:08:07 +0100

    Removed useless imports

--- microwave.py
+++ microwave.py
@@ -1,46 +1,11 @@
 import tkinter as tk
-import threading
-import simpleaudio as sa
-import traceback
-import os
-
-from tkinter import (
-    filedialog, 
-    messagebox, 
-    simpledialog
-)
-from PIL import (
-    Image,
-    ImageTk
-)
 
 # IMPORT HELPERS
-from Helpers.CurlHelper import (
-    get_curl_dir,
-    get_curl_files,
-    parse_curl_file,
-    execute_curl_bundle,
-    update_bearer_token,
-    extract_token
-)
-from Helpers.PopupHelper import (
-    show_error,
-    confirm,
-    ask_filename,
-    select_text_file,
-    create_modal,
-    open_file_editor,
-    open_choice_modal
-)
-from Helpers.LabelHelper import (
-    create_label,
-    destroy_labels
-)
+from Helpers.PopupHelper import open_choice_modal
+from Helpers.LabelHelper import create_label
 from Helpers.ImageHelper import create_image_model
-from Helpers.AudioHelper import AudioPlayer
 from Helpers.RequestHelper import on_click
 
-# GLOBALS
 options = []
 selected_curl_name = None
 
